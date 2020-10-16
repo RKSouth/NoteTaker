98d826d6026c536d5017df80bff7c2c38b2911eb
Angry Monster <67206311+RKSouth@users.noreply.github.com>
2020-10-15 19:17:45 -0700

    got everthing to work but delete

--- Develop/server.js
+++ Develop/server.js
@@ -11,48 +11,57 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Require routes file
-require('./routes/routes')(app);
+
+var noteTitles =[];
+
+// require('./routes/routes')(app);
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 // reservation.html
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-// // api/tables display all tables
-// app.get("/api/tables", function(req, res) {
-//     return res.json(tables);
-//   });
+app.get("/api/notes", function(req, res) {
+    return res.sendFile(path.join(__dirname, "db/db.json")); //path.json -> path.join
+  });
+
+// api/tables display all tables
+app.get("/api/notes", function(req, res) {
+    noteTitles = fs.readFileSync("db/db.json", "utf8");
+    noteTitles = JSON.parse(noteTitles);
+    return res.json(noteTitles);
+  });
 
 // // api/waitlist display all waitlist
 // app.get("/api/waitlist", function(req, res) {
 //     return res.json(waitlist);
 // });
 
-// // api/clear clear tables!
-// app.put("/api/clear", function(req, res) {
-//     tables = [];
-//     waitlist = [];
-//     res.send("clear")
-// })
+// api/clear clear tables!
+app.put("/api/clear", function(req, res) {
+    noteTitles = [];
+    res.send("clear")
+})
 
-// // api/delete/table
-// app.delete("/api/delete/table", function(req, res) {
-//     var index = req.body.index;
-//     var temp = [];
-//     for (var i = 0; i < tables.length; i++) {
-//         if (i !== parseInt(index)) {
-//           temp.push(tables[i]);
-//         }
-//     }
-//     tables = temp;
-//     res.send("table removed")
-// })
+// api/delete/table
+app.delete("/api/delete/table", function(req, res) {
+    var index = req.body.index;
+    var temp = [];
+    for (var i = 0; i < noteTitles.length; i++) {
+        if (i !== parseInt(index)) {
+          temp.push(noteTitles[i]);
+        }
+    }
+    noteTitles = temp;
+    res.send("table removed")
+})
 
 // // apie/delete/waitlist
 // app.delete("/api/delete/waitlist", function(req, res) {
@@ -67,19 +76,22 @@ app.get("/notes", function(req, res) {
 //     res.send("waitlist removed")
 // })
 
-// // post api/tables
-// app.post("/api/tables", function(req, res) {
-//     var newTable = req.body;
-//     console.log(newTable);
-//     if (tables.length < 5 ) {
-//         tables.push(newTable);
-//         res.send("Table")
-//     } else {
-//         waitlist.push(newTable);
-//         res.send();
-//     }
-// });
+// post api/tables
+app.post("/api/notes", function(req, res) {
+noteTitles =fs.readFileSync("db/db.json", "utf8");
+noteTitles= JSON.parse(noteTitles);
+req.body.id = noteTitles.length;
+noteTitles.push(req.body);
+noteTitles = JSON.stringify(noteTitles);
+fs.writeFile("db/db.json", noteTitles, "utf8", function(err) {
+    // error handling
+    if (err) throw err;
+  });
 
+});
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
 
 
 // Setup listener
